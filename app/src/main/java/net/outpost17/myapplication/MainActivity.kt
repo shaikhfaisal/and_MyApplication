754463e78723c79add264ec5f1c1c765377c1b3b
faisal <1963409+shaikhfaisal@users.noreply.github.com>
2018-05-31 00:36:53 +0100

    Added date picker dialog to record fasts in the past

--- app/src/main/java/net/outpost17/myapplication/MainActivity.kt
+++ app/src/main/java/net/outpost17/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package net.outpost17.myapplication
 
+import android.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,14 +10,37 @@ import android.widget.TextView
 import com.jakewharton.threetenabp.AndroidThreeTen
 import org.threeten.bp.LocalDate
 import android.arch.persistence.room.Room
+import android.widget.DatePicker
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.app.DialogFragment
+import java.util.*
 
 
-
+//class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+//
+//    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+         Use the current date as the default date in the picker
+//        val c = Calendar.getInstance()
+//        val year = c.get(Calendar.YEAR)
+//        val month = c.get(Calendar.MONTH)
+//        val day = c.get(Calendar.DAY_OF_MONTH)
+//
+         Create a new instance of DatePickerDialog and return it
+//        return DatePickerDialog(getActivity(), this, year, month, day)
+//    }
+//
+//    override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+         Do something with the date chosen by the user
+//    }
+//}
 
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var appLog:AppLog
+
+    private var selectedDate: LocalDate = LocalDate.now()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -45,26 +69,22 @@ class MainActivity : AppCompatActivity() {
 
     fun handleYesClick(view: View) {
 
-
-        val today = LocalDate.now()
-        appLog.didFastOn(today)
+        appLog.didFastOn(selectedDate)
 
         this.updateCounters()
 
-        Log.i("MyApp", "Clicked yes ")
+        Log.i("MyApp", "Clicked yes for " + selectedDate.toString())
         val mySnackbar = Snackbar.make(view, R.string.fasted_confirmation_text, Snackbar.LENGTH_SHORT)
         mySnackbar.show();
     }
 
     fun handleNoClick(view: View) {
 
-
-        val today = LocalDate.now()
-        appLog.missedFastOn(today)
+        appLog.missedFastOn(selectedDate)
 
         this.updateCounters()
 
-        Log.i("MyApp", "Clicked no ")
+        Log.i("MyApp", "Clicked no for " + selectedDate.toString())
 
         val mySnackbar = Snackbar.make(view, R.string.fasted_nonconfirmation_text, Snackbar.LENGTH_SHORT)
         mySnackbar.show();
@@ -78,6 +98,29 @@ class MainActivity : AppCompatActivity() {
         val totalNumberOfMissedFasts = appLog.getTotalNumberOfMissedFasts()
         findViewById<TextView>(R.id.fast_no_count).setText(totalNumberOfMissedFasts.toString())
 
+
+    }
+
+    fun showDatePickerDialog(v: View) {
+        Log.i("MyApp", "Clicked on calendar view")
+        val d = DatePickerDialog(
+                this,
+                android.R.style.Theme_Material_Light_Dialog,
+                dp_listener(),
+                LocalDate.now().year,
+                LocalDate.now().monthValue-1,
+                LocalDate.now().dayOfMonth
+        )
+        d.show()
+    }
+
+    inner class dp_listener(): DatePickerDialog.OnDateSetListener
+    {
+
+        override fun onDateSet(view: DatePicker, year: Int, month: Int, dayOfMonth: Int) {
+            selectedDate = LocalDate.of(year, month+1, dayOfMonth)
+            Log.i("MyApp", "Date Picker listener called with $year-$month-$dayOfMonth")
+        }
 
     }
 
