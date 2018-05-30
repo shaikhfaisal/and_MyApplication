ad66b0ed61db481cd612aa14ad3580e0a87c5b84
faisal <1963409+shaikhfaisal@users.noreply.github.com>
2018-05-31 00:39:34 +0100

    typos

--- app/src/main/java/net/outpost17/myapplication/MainActivity.kt
+++ app/src/main/java/net/outpost17/myapplication/MainActivity.kt
@@ -20,18 +20,18 @@ import java.util.*
 //class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
 //
 //    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
-         Use the current date as the default date in the picker
+//         Use the current date as the default date in the picker
 //        val c = Calendar.getInstance()
 //        val year = c.get(Calendar.YEAR)
 //        val month = c.get(Calendar.MONTH)
 //        val day = c.get(Calendar.DAY_OF_MONTH)
 //
-         Create a new instance of DatePickerDialog and return it
+//         Create a new instance of DatePickerDialog and return it
 //        return DatePickerDialog(getActivity(), this, year, month, day)
 //    }
 //
 //    override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
-         Do something with the date chosen by the user
+//         Do something with the date chosen by the user
 //    }
 //}
 
